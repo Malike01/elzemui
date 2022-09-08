78eaa4e7c2b17281f6aec33516cfd890d7e8b143
Malike01 <malikesen61@gmail.com>
2022-09-08 21:10:11 +0300

    elzemui library
    patika-dev library homework

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
-import { ExampleComponent } from 'elzemui'
+import { Button} from 'elzemui'
 import 'elzemui/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+function App() {
+  return (
+    <div>
+      <Button type='primary' onClick={() => alert("https://github.com/Malike01")} text="Primary Button"/> <br />
+      <Button type='secondary' onClick={() => alert("https://github.com/Malike01")} text="Default Button" /> <br />
+      <Button type='dashed' onClick={() => alert("https://github.com/Malike01")} text="Dashed Button" /> <br />
+      <Button type='link' onClick={() => alert("https://github.com/Malike01")} text="Link Button" /> <br />
+      <Button type='text' onClick={() => alert("https://github.com/Malike01")} text="Text Button" /> <br />
+    </div>
+  )
 }
 
 export default App
